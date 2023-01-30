63351eef89e375e9d6e9475307138affdd745ca6
BrVecchi <bno.vecchi@gmail.com>
2023-01-30 18:42:35 -0300

    refactor: sendStatus

--- src/controllers/payments-controller.ts
+++ src/controllers/payments-controller.ts
@@ -7,10 +7,6 @@ import ticketRepository from '@/repositories/payment-repository';
 export async function findPaymentInfo(req: AuthenticatedRequest, res: Response) {
     const {ticketId} = req.query
 
-    if (!ticketId) {
-        return res.sendStatus(httpStatus.BAD_REQUEST)
-    }
-
     try {
         const paymentInfo = (await ticketRepository.findUnique(Number(ticketId)))
         
@@ -20,22 +16,18 @@ export async function findPaymentInfo(req: AuthenticatedRequest, res: Response) 
 
         return res.status(httpStatus.OK).send(paymentInfo)
     } catch (error) {
-        return res.sendStatus(httpStatus.NOT_FOUND)
+        return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR)
     }
 }
 
 export async function createPayment(req: AuthenticatedRequest, res: Response) {
     const {ticketId, cardData} = req.body
 
-    if(!cardData) {
-        return res.sendStatus(httpStatus.BAD_REQUEST)
-    }
-
     try {
         const paymentData = (await ticketRepository.create(Number(ticketId), cardData))
         return res.status(httpStatus.OK).send(paymentData)
     } catch (error) {
         console.log(error)
-        return res.sendStatus(httpStatus.NOT_FOUND)
+        return res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR)
     }
 }
